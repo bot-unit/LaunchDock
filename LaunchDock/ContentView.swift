ee1c12964aefbff7f9c27cfc7d65e99007aa965c
Unit <admin@ghofi.net>
2025-09-22 16:01:55 +0200

    Initial Commit

---
+++ LaunchDock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaunchDock
+//
+//  Created by Igor Unit on 2025-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaunchDock/LaunchDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchDockApp.swift
+//  LaunchDock
+//
+//  Created by Igor Unit on 2025-09-22.
+//
+
+import SwiftUI
+
+@main
+struct LaunchDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
